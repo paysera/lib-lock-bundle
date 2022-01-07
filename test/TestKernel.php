b79e04956d34f0420e13ad2300db9b33a62d3395
Erikas Baranauskas <erikas.baranauskas@paysera.net>
2022-01-07 08:58:45 +0200

    tests fixed

--- test/TestKernel.php
+++ test/TestKernel.php
@@ -1,9 +1,11 @@
 <?php
 
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class TestKernel extends Kernel
+class TestKernel extends Kernel implements CompilerPassInterface
 {
     public function registerBundles()
     {
@@ -21,5 +23,10 @@ class TestKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__ . '/config.yml');
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        $container->getDefinition('paysera_lock.lock_manager')->setPublic(true);
     }
 }
